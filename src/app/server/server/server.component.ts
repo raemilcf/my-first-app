7304f1a63041021eece2f5ea441a8f4368ab082b
raemilcf <raemilcorniel@hotmail.com>
2023-02-25 21:20:51 -0400

    add button to servers, added info to server

--- src/app/server/server/server.component.ts
+++ src/app/server/server/server.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-server]',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
 
+    ServerName : string  = "Server Name";
+    ServerStatus : string = "Active";
+    ServerId : number = 1234;
+
+ngOnInit(){
+
+}
+
+    getServerStatus  (){
+      this.ServerStatus;
+
+    }
 }
